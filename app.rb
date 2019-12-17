58e165102212c466844fe5203b8cc056ece68c90
AnnaWijetunga <annawijetunga@gmail.com>
2019-12-17 18:27:24 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Anna."
+  end
+
+  get '/hometown' do
+    "My hometown is Westfield."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 100 Yard Dash."
+  end
+
 end
