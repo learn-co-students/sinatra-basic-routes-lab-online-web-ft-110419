7a100eadf5433fe14ac2fe273929b6be81e8be66
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-12-18 17:19:22 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+  "My name is Jess"
+  end
+
+  get '/hometown' do
+    "My hometown is Bayonne"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is currently I Am the Fire"
+  end
+
+  get '/' do
+  end
+
 end
