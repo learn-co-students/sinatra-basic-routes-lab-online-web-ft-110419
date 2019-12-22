c187f36ef2ee0dec5bc96d677758f3d0d554bc31
kchohee <kchohee0630@gmail.com>
2019-12-22 10:50:26 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+  
+    get("/name") { return "My name is Cho!" }
+    get("/hometown") { return "My hometown is Busan, S.Korea." }
+    get("/favorite-song") { return "My favorite song is anything from Jay Park!" }
+    
+  end
