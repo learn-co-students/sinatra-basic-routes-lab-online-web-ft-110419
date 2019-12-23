99eebf83672084df594b9a402ce89dcde98af704
LucyWeltner <lucyiweltner@gmail.com>
2019-12-23 10:40:41 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+	get '/name' do 
+		"My name is Lucy"
+	end
+	get '/hometown' do 
+		"My hometown is Concord, MA"
+	end
+	get '/favorite-song' do 
+		"My favorite song is 'Ziggy Starfish' by Open Mike Eagle"
+	end
 end
