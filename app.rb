956f5ff5665bacb52f0a5eb567591c7ce0287e45
Jeshua Hicks <jhicks7884@outlook.com>
2020-01-05 21:04:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/'do
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is jeshua!"
+  end
+
+  get '/hometown' do
+    "My hometown is Columbus!"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'blue on black'"
+  end
 end
