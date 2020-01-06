44f576431d91e2be065ea13b8e14e8b39b82239b
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-01-06 10:54:59 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        'My name is Rukshan Jonny Uddin'
+    end
+    get '/hometown' do
+        'My hometown is New York $hitty!'
+    end
+    get '/favorite-song' do
+        "My favorite song is 'Born to Die' by the Good... the Bad... the Leftover Crack!!!!!!!!!!!"
+    end    
 end
