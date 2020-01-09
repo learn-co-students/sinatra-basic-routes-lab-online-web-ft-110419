52fc58cd7c0c2005c774c64bff2d2459a134cc2f
nkizito92 <nkizito57@gmail.com>
2020-01-09 12:33:55 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do 
+        "Hello, World!"
+    end 
+
+    get '/name' do 
+        "My name is Kizito"
+    end 
+    get '/hometown' do 
+        "My hometown is Austin"
+    end 
+    get '/favorite-song' do 
+        "My favorite song is On the Low"
+    end 
 end
