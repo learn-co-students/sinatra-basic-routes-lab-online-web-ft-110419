5919236f98d2609f8185a370db74cb00351f2927
anahimana <alex.nahimana1@gmail.com>
2020-01-08 23:52:56 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
+# frozen_string_literal: true
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        'My name is Fred'
+    end
+    get '/hometown' do
+        'My hometown is somewhere special'
+    end
+    get '/favorite-song' do
+        'My favorite song is Hello'
+    end
 end
